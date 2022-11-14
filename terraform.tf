22a19ef5cf3b0df553d1fbc8ae002f16c60097eb
mohab4717 <107080258+mohab4717@users.noreply.github.com>
2022-11-15 01:26:02 +0200

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,8 +1,7 @@
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIAW2OXVUSBQEM7BMPV"
-  secret_key = "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"
+
 }
 
 resource "aws_instance" "myEC2" {
